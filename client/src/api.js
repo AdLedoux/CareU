1c367474f20cec8f4ce6fc889b73b84d96a9cf62
AdLedoux <adrien@familleledoux.fr>
2025-10-05 05:41:42 -0400

    Works ... by magic ....

--- client/src/api.js
+++ client/src/api.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
-const apiUrl = "/choreo-apis/awbo/backend/rest-api-be2/v1.0";
-
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
+  baseURL: process.env.REACT_APP_API_URL
 }); // Get the backend json api
 
 api.interceptors.request.use(
