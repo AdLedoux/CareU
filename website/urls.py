6ed9a6feb4f64654b80601b3c2fcb0229e67e6df
Godfred Kuwornu <godfred.kuwornu@mail.concordia.ca>
2025-10-11 23:10:13 -0400

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 
+
 from django.contrib import admin
 from django.urls import path, include
 
@@ -23,6 +24,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstapp.urls')),  # this hooks your app routes
 ]
+
+
+
 
 
 
