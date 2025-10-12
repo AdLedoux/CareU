3c6baf1b03bd227bee1c85a171bfa5d0a6160e9f
Godfred Kuwornu <godfred.kuwornu@mail.concordia.ca>
2025-10-11 23:05:44 -0400

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -14,15 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
-
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("firstapp.urls")),   # 👈 this connects firstapp/urls.py
-]
-
 
 
 from django.contrib import admin
@@ -32,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('firstapp.urls')),  # this hooks your app routes
 ]
+
 
 
