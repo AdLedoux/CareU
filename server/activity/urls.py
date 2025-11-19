06554a583907033bb62ad6726313feaa181ef0bd
Yuelin Wen <wenyuelinca@gmail.com>
2025-11-19 16:11:05 -0500

    Resolved merge conflicts

--- server/activity/urls.py
+++ server/activity/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import (
-    DailyActivityByIdView,
-    DailyCaloriesByIdView,
+    ActivityByUsernameView,
+    WeeklyActivitySummaryByUsername,
+    MonthlyActivitySummaryByUsername,
+    DailyCaloriesView,
+    WeeklyCaloriesView,
 )
 
 urlpatterns = [
-    path("activity/<int:id>/daily/", DailyActivityByIdView.as_view()),
-    path("calories/<int:id>/daily/", DailyCaloriesByIdView.as_view()),
+    path("user/<str:username>/activity/", ActivityByUsernameView.as_view()),
+    path("user/<str:username>/activity/weekly/", WeeklyActivitySummaryByUsername.as_view()),
+    path("user/<str:username>/activity/monthly/", MonthlyActivitySummaryByUsername.as_view()),
+    path("user/<str:username>/calories/daily/", DailyCaloriesView.as_view()),
+    path("user/<str:username>/calories/weekly/", WeeklyCaloriesView.as_view()),
 ]
