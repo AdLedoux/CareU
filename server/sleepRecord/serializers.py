cd51fcbe6fad62fd728c8fdd6cea323d4a971eb8
Yuelin Wen <wenyuelinca@gmail.com>
2025-11-20 13:08:45 -0500

    sleep UI + API done

--- server/sleepRecord/serializers.py
+++ server/sleepRecord/serializers.py
@@ -2,29 +2,22 @@ from rest_framework import serializers
 from .models import SleepRecord
 
 class SleepRecordSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source='user.username', read_only=True)
-
     class Meta:
         model = SleepRecord
         fields = [
             'id',
-            'username',
-            'user',
+            'Id',
             'SleepDay',
             'TotalSleepRecords',
             'TotalMinutesAsleep',
             'TotalTimeInBed'
         ]
-        extra_kwargs = {
-            'user': {'write_only': True} 
-        }
 
     def create(self, validated_data):
-        user = validated_data['user']
         sleep_day = validated_data['SleepDay']
 
         record, created = SleepRecord.objects.update_or_create(
-            user=user,
+            Id=validated_data['Id'],
             SleepDay=sleep_day,
             defaults={
                 'TotalSleepRecords': validated_data['TotalSleepRecords'],
