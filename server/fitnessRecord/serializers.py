0c95ca78e8cf5e95f4a407681b30ff210d46e289
Yuelin Wen <wenyuelinca@gmail.com>
2025-11-21 02:40:10 -0500

    update backend data model

--- server/fitnessRecord/serializers.py
+++ server/fitnessRecord/serializers.py
@@ -2,14 +2,13 @@ from rest_framework import serializers
 from .models import FitnessRecord
 
 class FitnessRecordSerializer(serializers.ModelSerializer):
+    Id = serializers.UUIDField(source='user.user_id', read_only=True)
     username = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = FitnessRecord
-        fields = ['id', 'username', 'user', 'ActivityHour', 'StepTotal']
-        extra_kwargs = {
-            'user': {'write_only': True}
-        }
+        fields = ['Id', 'username', 'user', 'ActivityHour', 'StepTotal']
+        extra_kwargs = {'user': {'write_only': True}}
 
     def create(self, validated_data):
         user = validated_data['user']
