f04da52b5d90999d5c10f5a35d738c3e16e07a92
Yuelin Wen <wenyuelinca@gmail.com>
2025-11-22 01:53:48 -0500

    set up ai with google gimini

--- server/website/settings.py
+++ server/website/settings.py
@@ -15,7 +15,10 @@ from datetime import timedelta
 from dotenv import load_dotenv
 import os
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
